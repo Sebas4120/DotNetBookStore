78d52384c9862bcd2d941bf847f078c5311ebb69
Sebas4120 <sebastianaguirrem49@gmail.com>
2024-05-30 06:39:12 -0400

    Jueves 6:38 _ ending part 1 of class

--- DotNetBookStore/Controllers/CategoriesController.cs
+++ DotNetBookStore/Controllers/CategoriesController.cs
@@ -1,16 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotNetBookStore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetBookStore.Controllers
 {
     public class CategoriesController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
+        {   
+            //paso 1: Aca creamos los datos en el Model
+
+            //Use the Category Model to generate 10 categories in memory to display in the view
+            //List list contains Model Category objects
+            var categories = new List<Category>();
+            //adding categories objects to this list
+            for (int i =1; i<11; i++)
+            {
+                categories.Add(new Category { CategoryId = i, Name = "Category " + i.ToString() });
+            }
+
+            //Paso 2: Ahora toca pasarle estos datos del Model a View
+            //must now pass the strongly-types data to the view 
+                //Aqui lo que estoy haciendo es pasar mi lista de categories que he creado a mi View
+            return View(categories);
         }
 
         public IActionResult Browse(string category)
-        {
+        {   
+            //This is the process to passing the value to my View
             ViewBag.Category = category; //ViewBag is a dynamic object, methd that send data from CONTROLLER Action Method to View
             return View();
         }
--- DotNetBookStore/Models/Category.cs
+++ DotNetBookStore/Models/Category.cs
@@ -3,7 +3,7 @@
     public class Category
     {
         //Primary key fields should always be called either {Model}Id or just Id
-        public int CategoryId { get; set; }
+        public int CategoryId { get; set; } // This is a primary key
 
         public string Name { get; set; }
     }
