4fb7e55e8965c8c7d61f94b7ff4369d68abba2c1
Sebas4120 <sebastianaguirrem49@gmail.com>
2024-05-31 10:31:58 -0400

    Friday 10:31 - I got to go to work

---
+++ DotNetBookStore/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace DotNetBookStore.Models
+{
+    public class Book
+    {
+        //Primary Key
+        public int BookId {  get; set; }
+
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        // We add '?' to make a variable nullable
+        public string? Image {  get; set; }
+
+        public bool MaturedContent
+    }
+}
--- DotNetBookStore/Models/Category.cs
+++ DotNetBookStore/Models/Category.cs
@@ -8,10 +8,11 @@ namespace DotNetBookStore.Models
         // The display name of CATEGORYID en Views se puede cambiar con DATA ANNOTATIONS
 
         //La DATA ANNOTATIONS se coloca encima del dato que quieres editar, en este caso, CategoryId es lo que queremos editar.
-        [Display(Name = "Category ID")]
+        //[Display(Name = "Category ID")]
 
-        //Para crear DATA VALIDATION is our MODEL!!!!
-        [Range(1,999999)]
+        //Para crear DATA VALIDATION is in our MODEL!!!!
+        //[Range(1,999999)]
+
         public int CategoryId { get; set; } // This is a primary key
         [Required(ErrorMessage ="A customized error message")]
         public string Name { get; set; }
